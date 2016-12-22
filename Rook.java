8a05b1be81841e1fc032beb338e0826145131ebf
WelcomeToJupiter <patteryanbert@gmail.com>
2016-12-21 22:35:35 -0500

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,3 +1,20 @@
 public class Rook extends Piece
 {
+public Rook(int x){
+		super.setImageIcon("images2/rook2.png");
+		super.setPlayer(x);
+	}
+	
+	public boolean isValidMove(Location from, Location to, Piece[][] b){
+		if(from.getRow()==to.getRow() && from.getColumn()!=to.getColumn()){
+			return true;
+		}
+		
+		if(from.getColumn()==to.getColumn() && from.getRow()!=to.getRow()){
+			return true;
+		}
+		
+		else
+			return false;
+	}
 }
