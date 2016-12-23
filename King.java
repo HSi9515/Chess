152e9951353514df0eec5ee5d9c4a7425abc05af
HSi9515 <sih9515@gmail.com>
2016-12-23 17:05:01 -0500

    King's isValidMove is complete
    The King can move a maximum vertical and horizontal displacement of 1 and cannot move into check.
    Also added toString method

--- King.java
+++ King.java
@@ -1,5 +1,3 @@
-
-
 public class King extends Piece{
 
 	public King(){
@@ -8,6 +6,33 @@ public class King extends Piece{
 	
 	public King(int player){
 		super(player, "king");
+	}
+	
+	public boolean isValidMove(Location from, Location to, Piece[][]b) {
+		if (b[to.row][to.column].getPlayer() == b[from.row][from.column].getPlayer()) {
+			System.out.println("    same player");
+			return false;
+		}
+		
+		else if (Math.abs(from.column - to.column) > 1 || Math.abs(from.row - to.row) > 1) {
+			System.out.println("    out of range");
+			return false;
+		}
+		
+		for (int i = 0; i < b.length; i++){
+			for (int j = 0; j < b[0].length; j++){
+				if (b[i][j].getPlayer() != this.getPlayer() && b[i][j].isValidMove(new Location(i, j), to, b)) {
+					System.out.println("    check by " + b[i][j] + " at "+ i  + ","+ j);
+					return false;
+				}
+			}
+		}
+			
+		return true;
+	}
+	
+	public String toString(){
+		return super.toString() + ": King";
 	}
 	
 }
