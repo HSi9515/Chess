168effcdffac1f940dba6c0e934255964469201b
HSi9515 <sih9515@gmail.com>
2017-01-05 09:01:39 -0500

    Fixed Bug
    rowToCheck has been corrected

--- Queen.java
+++ Queen.java
@@ -13,15 +13,14 @@ public class Queen extends Piece{
 	
 	public boolean isValidMove(Location from, Location to, Piece[][]b){
 		if (b[to.row][to.column].getPlayer() == b[from.row][from.column].getPlayer()) {
-			System.out.println("same player");
+			System.out.println("    same player");
 			return false;
 		}
-			
 		
 		else if (from.row == to.row){
 			for (int i = Math.min(from.column, to.column) + 1; i < Math.max(from.column, to.column); i++){
 				if (b[from.row][i].getPlayer() == 1 || b[from.row][i].getPlayer() == 2) {
-					System.out.println("same row");
+					System.out.println("    same row");
 					return false;
 				}
 			}
@@ -31,7 +30,7 @@ public class Queen extends Piece{
 		else if (from.column == to.column){
 			for (int i = Math.min(from.row, to.row) + 1; i < Math.max(from.row, to.row); i++){
 				if (b[i][from.column].getPlayer() == 1 || b[i][from.column].getPlayer() == 2){
-					System.out.println("same column");
+					System.out.println("    same column");
 					return false;
 				}
 			}
@@ -42,11 +41,11 @@ public class Queen extends Piece{
 			int rowToCheck, columnToCheck;
 			
 			for (int i = 1; i < Math.abs(from.row-to.row); i++){
-				rowToCheck = Math.min(from.row, to.row) + i;
+				rowToCheck = from.row + i*(int)Math.signum(to.row-from.row);
 				columnToCheck = from.column + i*(int)Math.signum(to.column-from.column);
 				
 				if (b[rowToCheck][columnToCheck].getPlayer() == 1 || b[rowToCheck][columnToCheck].getPlayer() == 2) {
-					System.out.println("same diagonal");
+					System.out.println("   same diagonal");
 					return false;
 				}
 			}
